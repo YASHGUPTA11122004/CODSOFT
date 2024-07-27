1ed0ec70d73208006ca6d251756badbb59c7014d
Yash Gupta <148563569+YASHGUPTA11122004@users.noreply.github.com>
2024-07-27 09:48:18 +0530

    Add files via upload
    Number Guessing Game in C++
    This repository contains a simple number guessing game implemented in C++. The game prompts the user to guess a randomly generated number within a specified range. It provides feedback on whether the guess is too high, too low, or correct, and keeps track of the number of attempts made. Ideal for beginners to practice basic C++ programming concepts such as loops, conditionals, and random number generation.

---
+++ NumberGuessingGame.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+
+    int randomNumber = std::rand() % 100 + 1;
+    int userGuess = 0;
+
+    std::cout << "Hello and Welcome to the Number Guessing Game!" << std::endl;
+    std::cout << "I've produced a random number between 1 and 100." << std::endl;
+    std::cout << "Can you guess what it is?" << std::endl;
+
+    while (true) {
+        std::cout << " Please enter your guess: ";
+        std::cin >> userGuess;
+
+        if (userGuess > randomNumber) {
+            std::cout << "Too high! Try again." << std::endl;
+        } else if (userGuess < randomNumber) {
+            std::cout << "Too low! Try again." << std::endl;
+        } else {
+            std::cout << "Congratulations! You guessed the correct number!" << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+}
